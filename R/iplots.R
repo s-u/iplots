7611e2fa0b613524d49ddea1f24f09dbbfff9c8e
Simon Urbanek <simon.urbanek@R-project.org>
2003-04-07 15:26:43 +0000

    rename fw to .iplots.fw (was not included in previous commit!) added a few comments and low-level functions
    git-svn-id: svn://svn.rforge.net/iplots/trunk@8 8e282063-7124-4b67-9d11-cc4721fc7695

--- R/iplots.R
+++ R/iplots.R
@@ -1,26 +1,36 @@
 .packageName <- "iplots"
 
+# -- global variables:
+# .iplots.fw     - framework object
+# .iplots        - list of iplot objects
+# .iplot.curid   - current plot ID
+# .iplot.current - .iplots[[.iplot.curid]]
+
 library(SJava);
 .First.lib <- function(lib, pkg) {
   cp<-paste(lib,pkg,"cont","iplots.jar",sep=.Platform$file.sep)
   .JavaInit(list(classPath=c(cp)))
-  fw<<-.JavaConstructor("Framework")
+  .iplots.fw<<-.JavaConstructor("Framework")
 }
 
-.di <- function() { .JavaInit(list(classPath=".")); fw<<-.JavaConstructor("Framework") }
+.di <- function() { .JavaInit(list(classPath=".")); .iplots.fw<<-.JavaConstructor("Framework") }
 
 ivar.new <- function (name,cont) {
   if(is.factor(cont)) {
-    v<-.Java(fw,"newVar",name,as.character(cont))
+    v<-.Java(.iplots.fw,"newVar",name,as.character(cont))
     if (v>=0) {
-      .Java(.Java(fw,"getVar",v),"categorize",TRUE)
+      .Java(.Java(.iplots.fw,"getVar",v),"categorize",TRUE)
     }
     return(v)
   }
-  .Java(fw,"newVar",name,cont)
+  .Java(.iplots.fw,"newVar",name,cont)
 }
 
-ivar.update <- function (vid,cont) { .Java(fw,"replaceVar",vid,cont); .Java(fw,"updateVars"); }
+ivar.update <- function (vid,cont) { .Java(.iplots.fw,"replaceVar",vid,cont); .Java(.iplots.fw,"updateVars"); }
+
+ivar <- function (vid) { iset.var(vid) }
+
+# iplot managemen functions
 
 iplot.set <- function (which=iplot.next()) {
   a<-try(.iplots[[which<-as.integer(which)]])
@@ -31,10 +41,6 @@ iplot.set <- function (which=iplot.next()) {
     .iplot.curid<<-which
   }
 }
-
-ivar <- function (vid) { iset.var(vid) }
-
-# iplot managemen functions
 
 iplot.list <- function () {
   if (exists(".iplots")) .iplots else list()
@@ -58,41 +64,36 @@ iplot.prev <- function(which = iplot.cur()) {
  ((which-2)%%length(.iplots))+1
 }
 
-iplot.set <- function(which=iplot.next()) {
-  if (which>=0 && which<length(.iplots)) 
-    .iplot.current <<- .iplots[[.iplot.curid <<- which]];
-}
-
 # generic print for i[lots
 print.iplot <- function(p) { cat("ID:",p$id," Name: \"",attr(p,"iname"),"\"\n",sep="") }
 
 # low-level plot calls
 
-iplot.iPlot <- function (v1,v2) { iplot.new(lastPlot<<-.Java(fw,"newScatterplot",v1,v2)) }
-iplot.iHist <- function (v1) { iplot.new(lastPlot<-.Java(fw,"newHistogram",v1)) }
-iplot.iBar  <- function (v1) { iplot.new(lastPlot<-.Java(fw,"newBarchart",v1)) }
+iplot.iPlot <- function (v1,v2) { iplot.new(lastPlot<<-.Java(.iplots.fw,"newScatterplot",v1,v2)) }
+iplot.iHist <- function (v1) { iplot.new(lastPlot<-.Java(.iplots.fw,"newHistogram",v1)) }
+iplot.iBar  <- function (v1) { iplot.new(lastPlot<-.Java(.iplots.fw,"newBarchart",v1)) }
 
 # user-level plot calls
 
 iplot <- function(v1,v2) {
-   if ((is.vector(v1) || is.factor(v1)) && length(v1)>1) v1<-ivar.new(.Java(fw,"getNewTmpVar",as.character(deparse(substitute(v1)))),v1);
-   if ((is.vector(v2) || is.factor(v2)) && length(v2)>1) v2<-ivar.new(.Java(fw,"getNewTmpVar",as.character(deparse(substitute(v2)))),v2);
+   if ((is.vector(v1) || is.factor(v1)) && length(v1)>1) v1<-ivar.new(.Java(.iplots.fw,"getNewTmpVar",as.character(deparse(substitute(v1)))),v1)
+   if ((is.vector(v2) || is.factor(v2)) && length(v2)>1) v2<-ivar.new(.Java(.iplots.fw,"getNewTmpVar",as.character(deparse(substitute(v2)))),v2)
    iplot.iPlot(v1,v2)
 }
 
 ibar <- function(v1) {
-   if ((is.vector(v1) || is.factor(v1)) && length(v1)>1) v1<-ivar.new(.Java(fw,"getNewTmpVar",as.character(deparse(substitute(v1)))),v1);
+   if ((is.vector(v1) || is.factor(v1)) && length(v1)>1) v1<-ivar.new(.Java(.iplots.fw,"getNewTmpVar",as.character(deparse(substitute(v1)))),v1);
    iplot.iBar(v1)
 }
 
 ihist <- function(v1) {
-   if ((is.vector(v1) || is.factor(v1)) && length(v1)>1) v1<-ivar.new(.Java(fw,"getNewTmpVar",as.character(deparse(substitute(v1)))),v1);
+   if ((is.vector(v1) || is.factor(v1)) && length(v1)>1) v1<-ivar.new(.Java(.iplots.fw,"getNewTmpVar",as.character(deparse(substitute(v1)))),v1);
    iplot.iHist(v1)
 }
 
 # old API functions
 
-iplot.showVars <- function() { .Java(fw,"ivar.newFrame"); }
+iplot.showVars <- function() { .Java(.iplots.fw,"ivar.newFrame"); }
 iplot.setXaxis <- function(ipl=lastPlot,x1,x2) { .Java(.Java(ipl,"getXAxis"),"setValueRange",x1,x2-x1); }
 iplot.setYaxis <- function(ipl=lastPlot,x1,x2) { .Java(.Java(ipl,"getYAxis"),"setValueRange",x1,x2-x1); }
 iplot.resetXaxis <- function(ipl=lastPlot) { .Java(.Java(ipl,"getXAxis"),"setDefaultRange"); }
@@ -103,39 +104,39 @@ iset.df <- function(df) { ndf<-list(); for(i in names(df)) { ndf[[i]]<-ivar.new(
 # selection/highlighting API
 
 iset.select <- function(what,mark=TRUE) {
-  m<-.Java(.Java(fw,"getCurrentSet"),"getMarker")
+  m<-.Java(.Java(.iplots.fw,"getCurrentSet"),"getMarker")
   if (is.logical(what)) {
     for(i in 1:length(what)) { if (what[i]) .Java(m,"set",as.integer(i-1),mark) }
   } else {
     for(i in what) { .Java(m,"set",as.integer(i-1),mark) }
   }
-  .Java(fw,"updateMarker")
+  .Java(.iplots.fw,"updateMarker")
 }
 
 iset.selected <- function() {
   v<-vector()
-  m<-.Java(.Java(fw,"getCurrentSet"),"getMarker")
+  m<-.Java(.Java(.iplots.fw,"getCurrentSet"),"getMarker")
   numc<-.Java(m,"size")
   for(i in 1:numc) { if(.Java(m,"get",as.integer(i-1))<0) v<-c(v,i) }
   v;
 }
 
-iset.selectAll <- function() { .Java(.Java(.Java(fw,"getCurrentSet"),"getMarker"),"selectAll",as.integer(1)); .Java(fw,"updateMarker"); }
-iset.selectNone <- function() { .Java(.Java(.Java(fw,"getCurrentSet"),"getMarker"),"selectNone"); .Java(fw,"updateMarker"); }
+iset.selectAll <- function() { .Java(.Java(.Java(.iplots.fw,"getCurrentSet"),"getMarker"),"selectAll",as.integer(1)); .Java(.iplots.fw,"updateMarker"); }
+iset.selectNone <- function() { .Java(.Java(.Java(.iplots.fw,"getCurrentSet"),"getMarker"),"selectNone"); .Java(.iplots.fw,"updateMarker"); }
 
 # brushing API
 
 iset.brush <- function(col) {
   if (is.numeric(col) && !is.integer(col)) col<-as.integer(col)
   if (is.factor(col)) col<-as.integer(as.integer(col)+1)
-  .Java(fw,"setSecMark",col);
+  .Java(.iplots.fw,"setSecMark",col);
 }
 
-iset.updateVars <- function() { .Java(fw,"updateVars"); }
+iset.updateVars <- function() { .Java(.iplots.fw,"updateVars"); }
 
 iset.var <- function(vid) {
   vid<-as.integer(vid)
-  if(.Java(fw,"varIsNum",vid)!=0) .Java(fw,"getDoubleContent",vid) else as.factor(.Java(fw,"getStringContent",vid))
+  if(.Java(.iplots.fw,"varIsNum",vid)!=0) .Java(.iplots.fw,"getDoubleContent",vid) else as.factor(.Java(.iplots.fw,"getStringContent",vid))
 }
 
 # iobj API
@@ -166,6 +167,12 @@ iobj.get <- function(pos, plot = .iplot.current) {
   a<-list(obj=.Java(pm,"get",as.integer(pos-1)),pm=pm,plot=plot)
   class(a)<-"iobj"
   a
+}
+
+.iplot.get.by.pm <-function (which) { # get plot from the list by pm entry
+  for (i in .iplots)
+    if (i$pm == which) return(i)
+  NULL;
 }
 
 iobj.rm <- function(obj) {
@@ -207,6 +214,7 @@ ilines <- function(x,y,col=NULL,fill=NULL) {
       iobj.color(pp,col,fill) # includes "update"
     else
       .Java(pp$obj,"update")
+    pp
   }
 }
 
