e601d4ae0a089235418e75a9101d32a38f3c05d1
Alexander Gouberman <alexander.gouberman@unibw.de>
2006-06-12 21:20:38 +0000

    updated some methods from custom plots
    git-svn-id: svn://svn.rforge.net/iplots/trunk@86 8e282063-7124-4b67-9d11-cc4721fc7695

--- R/iplots.R
+++ R/iplots.R
@@ -969,8 +969,15 @@ iplot.getGrDevID<-function() {
 		if(iplot.grdevice=="OPENGL") return(2);
 	}
 	
-new.PPrimRect<-function(pplot,x,y,w,h,ids) {
-	pp=.jnew("org/rosuda/ibase/toolkit/PPrimRectangle");
+## (x,y) top left corner of rectangle
+## (w,h) width, height of rectangle
+new.PPrimRect<-function(pplot,x1,y1,x2,y2,ids) {
+	or=1;
+	if(w<0 && h>=0) {or=4;}
+	else if(w>=0 && h<0) {or=3;}
+	else if(w>=0 && h>=0) {or=1;}
+	else {or=2;}
+	pp=.jnew("org/rosuda/ibase/toolkit/PPrimRectangle",as.integer(or));
 	.jcall(pp,,"setBounds",as.double(x),as.double(y),as.double(w),as.double(h));
 	.jcall(pp,,"setCaseIDs",.jarray(ids));
 	.iplots.cp.addPP(pplot,pp);
@@ -986,12 +993,17 @@ new.PPrimRect<-function(pplot,x,y,w,h,ids) {
 	.jcall(pplot$obj,,"resetPP");
 }
 
-iaxiscont<-function(pplot,n,or,valuerange,pixelcoord) {
+iaxiscont<-function(pplot,n,or,valuerange,pixelcoord,...) {
 	if(length(valuerange)!=2&&length(pixelcoord)!=2) return(NULL);
 	if(or=="x") or=0;
 	if(or=="y") or=1;
 	axtype=0;
-	a=.jnew("org/rosuda/ibase/toolkit/Axis",.jnull("org/rosuda/ibase/SVar"),as.integer(or),as.integer(axtype));
+	svar=.jnull("org/rosuda/ibase/SVar");
+	if (length(list(...))>0) {
+		l<-list(...);
+		svar=ivar.new("Var Name",l[[1]])$obj;
+	}
+	a=.jnew("org/rosuda/ibase/toolkit/Axis",svar,as.integer(or),as.integer(axtype));
 	.jcall(a,,"setGeometry",as.integer(or),as.integer(pixelcoord[1]),as.integer(pixelcoord[2]));
 	.jcall(a,"Z","setValueRange",as.double(valuerange[1]),as.double(valuerange[2]));
 	.jcall(pplot$obj,,"setAxis",a);
@@ -1080,22 +1092,28 @@ iagepyr.definition=list(name="Alterspyramide",min.data.dim=2,param=list(bin.widt
     		bins=ceiling(max(data[[1]])/bin.width)
 		    bin=ceiling((data[[1]]-anchor)/bin.width)
 		   	largest.bin=max(table(bin))
-		   	print(largest.bin);
-	   	    print(plot$obj);
-	   	    a.l=iaxiscont(plot, 1, "x", c(0, largest.bin), c(width/2, 10))
+
+##          v1<-c()
+##			for(i in seq(1,length(left))) v1<-c(v1,data[[left[i],1]])
+##			v2<-c()
+##			for(i in seq(1,length(right))) v2<-c(v2,data[[right[i],1]])
+##	   	    a.l=iaxiscont(plot, 1, "x", c(0, largest.bin), c(width/2, 10), v1)
+## 	        a.r=iaxiscont(plot, 2, "x", c(0, largest.bin), c(width/2, width-10), v2)
+
+   	   	    a.l=iaxiscont(plot, 1, "x", c(0, largest.bin), c(width/2, 10))
    	        a.r=iaxiscont(plot, 2, "x", c(0, largest.bin), c(width/2, width-10))
             a.y=iaxiscont(plot, 3, "y", c(0, bins), c(0,100))
+            
 			.iplots.cp.resetPP(plot);
             for (i in 1:bins) {
 	        	ids.l=which(bin==i & side==1)
-	        	str(ids.l);
-	        	if (length(ids.l)>0)
-	        		new.PPrimRect(plot, a.l(0), a.y(i-0.5), a.l(length(ids.l)), a.y(i+0.5), id=ids.l)
 	        	ids.r=which(bin==i & side==2)
-     	    	if (length(ids.r)>0)
-	        		new.PPrimRect(plot, a.r(0), a.y(i-0.5), a.r(length(ids.r)), a.y(i+0.5), id=ids.r)
+     	    	if (length(ids.l)>0)
+	        		new.PPrimRect(plot, a.l(length(ids.l))-a.l(0), a.y(i+0.5)-a.y(i-0.5), a.l(0), a.y(i-0.5),id=ids.l)
+	        	if (length(ids.r)>0)
+	        		new.PPrimRect(plot, a.r(0), a.y(i-0.5), a.r(length(ids.r))-a.r(0), a.y(i+0.5)-a.y(i-0.5), id=ids.r);
+	        		
 		    }
 		});
 
 iagepyr<-icustom.plot(iagepyr.definition);
-		
