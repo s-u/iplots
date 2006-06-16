8faae5963e3a1a129273b39497e3427cd915f1a2
Alexander Gouberman <alexander.gouberman@unibw.de>
2006-06-16 10:00:58 +0000

    added independency from customcanvas updated axis orientation
    git-svn-id: svn://svn.rforge.net/iplots/trunk@93 8e282063-7124-4b67-9d11-cc4721fc7695

--- R/icustom.R
+++ R/icustom.R
@@ -9,15 +9,17 @@ iplot.getGrDevID<-function() {
 		if(iplot.grdevice=="SWING") return(1);
 		if(iplot.grdevice=="OPENGL") return(2);
 	}
-	
+
 ## (x,y) top left corner of rectangle
 ## (w,h) width, height of rectangle
 new.PPrimRect<-function(pplot,x,y,w,h,or,ids) {
 	pp=.jnew("org/rosuda/ibase/toolkit/PPrimRectangle",as.integer(or));
+	if (w<0) { x<-x+w; w<--w }
+	if (h<0) { y<-y+h; h<--h }
 	.jcall(pp,,"setBounds",as.double(x),as.double(y),as.double(abs(w)),as.double(abs(h)));
 	.jcall(pp,,"setCaseIDs",.jarray(ids));
 	.iplots.cp.addPP(pplot,pp);
-	print(pp);
+	#print(pp);
 	return(pp);
 }
 
@@ -29,8 +31,17 @@ new.PPrimRect<-function(pplot,x,y,w,h,or,ids) {
 	.jcall(pplot$obj,,"resetPP");
 }
 
-iaxiscont<-function(pplot,n,or,valuerange,pixelcoord,...) {
-	if(length(valuerange)!=2&&length(pixelcoord)!=2) return(NULL);
+.iplots.cp.resetAxes<-function(pplot) {
+	.jcall(pplot$obj,,"resetAxes");
+}
+
+.iplots.cp.setBorderColorSel<-function(pplot,pp) {
+	.jcall(pplot$obj,,"setColors",pp);
+}
+
+## trl==translattion in orthogonal direction
+iaxiscont<-function(pplot,n,or,valuerange,pxc,trl=0,drawAxis=T,...) {
+	if(length(valuerange)!=2&&length(pxc)!=2) return(NULL);
 	if(or=="x") or=0;
 	if(or=="y") or=1;
 	axtype=0;
@@ -40,10 +51,11 @@ iaxiscont<-function(pplot,n,or,valuerange,pixelcoord,...) {
 		svar=ivar.new("Var Name",l[[1]])$obj;
 	}
 	a=.jnew("org/rosuda/ibase/toolkit/Axis",svar,as.integer(or),as.integer(axtype));
-	.jcall(a,,"setGeometry",as.integer(or),as.integer(pixelcoord[1]),as.integer(pixelcoord[2]));
+	.jcall(a,,"setGeometry",as.integer(or),as.integer(pxc[1]),as.integer(pxc[2]));
 	.jcall(a,"Z","setValueRange",as.double(valuerange[1]),as.double(valuerange[2]));
-	.jcall(pplot$obj,,"setAxis",a);
+	.jcall(pplot$obj,,"setAxis",a,as.integer(trl),drawAxis);
 	.jcall(a,,"addDepend",.jcast(pplot$obj,"org/rosuda/ibase/Dependent"));
+	print(a);
 	gvp=function(pos) {
 		ppos=.jcall(a,"I","getValuePos",as.double(pos));
 		return(ppos);
@@ -84,16 +96,12 @@ iaxis<-function(pplot,n,or,valuerange,pixelcoord) {
 	return(gcp);
 }
 
-data(iris);
-dat=iris[1:2];
-dat[[2]]=factor(as.integer(runif(150,0,2)));levels(dat[[2]])<-c("M","W")
-dat[[1]]=dat[[1]]*13-30
-
-pplot=.iplots[[iplot.cur()]];
+.ic.plots<-new.env(baseenv())
 
 icustom.plot<-function(name,min.data.dim,param,construct) {
-	
-	f<-function(vars,...) {
+	.ic.plots[[name]]=list(plots=new.env(baseenv()),construct=construct)
+	# real params: vars, ..., name
+	f<-function() {
 	  vv<-vector()
 	  i <- 1
 	  for (var in vars) {
@@ -107,62 +115,61 @@ icustom.plot<-function(name,min.data.dim,param,construct) {
 	    }
 	    i <- i+1
  	 }
-	  if (length(list(...))>0) iplot.opt(...,plot=a)
-		lastPlot<-.jcall(.iplots.fw,"Lorg/rosuda/ibase/plots/CustomCanvas;","newCustomplot",as.integer(vv),"");
+ 	 id<-1
+ 	 if (length(ls(.ic.plots[[name]]$plots))>0) id<-max(as.integer(ls(.ic.plots[[name]]$plots)))+1
+		lastPlot<-.jcall(.iplots.fw,"Lorg/rosuda/ibase/plots/CustomCanvas;","newCustomplot",as.integer(vv),
+				paste('.ic.plots[["',name,'"]]',sep=''),as.character(id))
 		
-		a<-iplot.new(lastPlot);
-		return(a);
+	 a<-iplot.new(lastPlot);
+	 e<-new.env(parent=.GlobalEnv)
+	 e$construct<-construct
+	 environment(e$construct)<-e
+	 e$plot<-a
+	 e$data<-vars 
+	 for (x in names(param)) e[[x]]<-param[[x]]
+	 if (length(list(...))>0) #iplot.opt(...,plot=a)
+	   { l<-list(...); for (i in names(l)) e[[i]]<-l[[i]] }
+ 	 .ic.plots[[name]]$plots[[as.character(id)]]<-e
 	}
-	
+	formals(f)<-c(alist(vars=,...=),list(name=name))
+	f
 }
 
-iagepyr.caller<-function(plot,width,height,data) {
-	iagepyr.definition$construct(plot,width,height,data);
-}
-
-iagepyr.definition=list(name="Alterspyramide",min.data.dim=2,param=list(bin.width=10),
-		construct=function(plot,width,height,data)	 {
-			print("Bin im construct");
-			bin.width=iagepyr.definition$param$bin.width;
+iagepyr<-icustom.plot(name="Alterspyramide",min.data.dim=2,param=list(bin.width=10),
+		construct=function(width,height)	 {
 		    side=unclass(data[[2]])
 		    left=which(side==1)
 		    right=which(side==2)
    			anchor=min(data[[1]])
     		bins=ceiling(max(data[[1]])/bin.width)
 		    bin=ceiling((data[[1]]-anchor)/bin.width)
-		   	largest.bin=max(table(bin))
+		   	l.largest.bin=max(table(bin));
+		   	r.largest.bin=l.largest.bin;
 
-##          v1<-c()
-##			for(i in seq(1,length(left))) v1<-c(v1,data[[left[i],1]])
-##			v2<-c()
-##			for(i in seq(1,length(right))) v2<-c(v2,data[[right[i],1]])
-##	   	    a.l=iaxiscont(plot, 1, "x", c(0, largest.bin), c(width/2, 10), v1)
-## 	        a.r=iaxiscont(plot, 2, "x", c(0, largest.bin), c(width/2, width-10), v2)
+			.iplots.cp.resetAxes(plot);
+   	        a.r=iaxiscont(plot, 1, "x", c(0, r.largest.bin), c(width/2, width-10-width/2),height-10)
+   	   	    a.l=iaxiscont(plot, 2, "x", c(0, l.largest.bin), c(width/2, 10-width/2),height-10)
+            a.y=iaxiscont(plot, 3, "y", c(0, bins), c(height-10,-height+30),width/2)
 
-   	   	    a.l=iaxiscont(plot, 1, "x", c(0, largest.bin), c(width/2, 10))
-   	        a.r=iaxiscont(plot, 2, "x", c(0, largest.bin), c(width/2, width-10))
-            a.y=iaxiscont(plot, 3, "y", c(0, bins), c(0,100))
-            
 			.iplots.cp.resetPP(plot);
             for (i in 1:bins) {
-	        	ids.l=which(bin==i & side==1)
-	        	ids.r=which(bin==i & side==2)
+	        	ids.l=which(bin==i & side==1)-1
+	        	ids.r=which(bin==i & side==2)-1
+	        	
      	    	if (length(ids.l)>0) {
-     	    		x=2*a.l(0)-a.l(length(ids.l));
+     	    		x=a.l(0);
      	    		y=a.y(i-0.5);
      	    		w=a.l(length(ids.l))-a.l(0);
      	    		h=a.y(i+0.5)-a.y(i-0.5);
-					new.PPrimRect(plot,x,y,w,h, as.integer(3), id=ids.l);
+					pp<-new.PPrimRect(plot,x,y,w,h, as.integer(3), id=ids.l);
 				}
 				if(length(ids.r)>0) {
 					x=a.r(0);
 					y=a.y(i-0.5);
 					w=a.r(length(ids.r))-a.r(0);
 					h=a.y(i+0.5)-a.y(i-0.5);
-					new.PPrimRect(plot,x,y,w,h,as.integer(1),id=ids.r);
+					pp<-new.PPrimRect(plot,x,y,w,h,as.integer(1),id=ids.r);
 				}
-        		
+
 		    }
 		});
-
-iagepyr<-icustom.plot(iagepyr.definition);
