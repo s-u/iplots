579411aafc5d649f96cbe9299f4ffcc93e14301d
Simon Urbanek <simon.urbanek@R-project.org>
2006-06-29 18:30:47 +0000

    fixes in ivar.update and ihist; remove icustom stuff
    git-svn-id: svn://svn.rforge.net/iplots/trunk@100 8e282063-7124-4b67-9d11-cc4721fc7695

--- R/iplots.R
+++ R/iplots.R
@@ -221,18 +221,18 @@ ivar.update <- function (var, cont) {
   if (!inherits(var, "ivar"))
     stop("invalid variable")
   if (is.factor(cont))
-    .jcall(.iplots.fw, "Lorg/rosuda/ibase/SVar;", "replaceVar",
+    .jcall(.iplots.fw, "I", "replaceVar",
            var$vid, as.integer(unclass(cont)-1), levels(cont))
   else if (is.numeric(cont) || is.character(cont))
-    .jcall(.iplots.fw, "Lorg/rosuda/ibase/SVar;", "replaceVar", var$vid, cont)
+    .jcall(.iplots.fw, "I", "replaceVar", var$vid, cont)
   else {
       vf <- factor(as.character(cont))
-      .jcall(.iplots.fw, "Lorg/rosuda/ibase/SVar;", "replaceVar",
+      .jcall(.iplots.fw, "I", "replaceVar",
              var$vid, as.integer(unclass(vf)-1), levels(vf))
     }
 }
 
-`<-.ivar` <- ivar.update
+#`<-.ivar` <- ivar.update
 
 #==========================================================================
 # iplot management functions
@@ -449,14 +449,14 @@ ibar <- function(var, ...) {
 }
 
 ihist <- function(var, ...) {
-  if (!is.numeric(var))
-    stop("Variable must be numeric.")
   len<-length(var)
   if (inherits(var,"ivar")) len<-.jcall(var$obj,"I","size")
   if (len<2)
     stop("ihist requires at least two data points")
-   if ((is.vector(var) || is.factor(var)) && length(var)>1)
-     var<-ivar.new(.ivar.valid.name(deparse(substitute(var))[1]), var);
+#  if (!is.numeric(var))
+#    stop("Variable must be numeric.")
+  if (!inherits(var, "ivar"))
+    var<-ivar.new(.ivar.valid.name(deparse(substitute(var))[1]), var);
   .iplot.iHist(var, ...)
 }
 
@@ -972,175 +972,3 @@ iset.sel.changed <- function (iset=iset.cur()) {
 .iDebug <- function(level=1) {
   .jcall(.iplots.fw,"V","setDebugLevel",as.integer(level))
 }
-
-
-
-
-##################################
-# added extended functions
-# have to be ordered in list above
-##################################
-
-
-iplot.getGrDevID<-function() {
-		if(iplot.grdevice=="AWT") return(0);
-		if(iplot.grdevice=="SWING") return(1);
-		if(iplot.grdevice=="OPENGL") return(2);
-	}
-	
-## (x,y) top left corner of rectangle
-## (w,h) width, height of rectangle
-new.PPrimRect<-function(pplot,x,y,w,h,or,ids) {
-	pp=.jnew("org/rosuda/ibase/toolkit/PPrimRectangle",as.integer(or));
-	.jcall(pp,,"setBounds",as.double(x),as.double(y),as.double(abs(w)),as.double(abs(h)));
-	.jcall(pp,,"setCaseIDs",.jarray(ids));
-	.iplots.cp.addPP(pplot,pp);
-	print(pp);
-	return(pp);
-}
-
-.iplots.cp.addPP<-function(pplot,pp) {
-	.jcall(pplot$obj,,"addPP",pp);
-}
-
-.iplots.cp.resetPP<-function(pplot) {
-	.jcall(pplot$obj,,"resetPP");
-}
-
-iaxiscont<-function(pplot,n,or,valuerange,pixelcoord,...) {
-	if(length(valuerange)!=2&&length(pixelcoord)!=2) return(NULL);
-	if(or=="x") or=0;
-	if(or=="y") or=1;
-	axtype=0;
-	svar=.jnull("org/rosuda/ibase/SVar");
-	if (length(list(...))>0) {
-		l<-list(...);
-		svar=ivar.new("Var Name",l[[1]])$obj;
-	}
-	a=.jnew("org/rosuda/ibase/toolkit/Axis",svar,as.integer(or),as.integer(axtype));
-	.jcall(a,,"setGeometry",as.integer(or),as.integer(pixelcoord[1]),as.integer(pixelcoord[2]));
-	.jcall(a,"Z","setValueRange",as.double(valuerange[1]),as.double(valuerange[2]));
-	.jcall(pplot$obj,,"setAxis",a);
-	.jcall(a,,"addDepend",.jcast(pplot$obj,"org/rosuda/ibase/Dependent"));
-	gvp=function(pos) {
-		ppos=.jcall(a,"I","getValuePos",as.double(pos));
-		return(ppos);
-	}
-	return(gvp);
-}
-
-iaxiscat<-function(pplot,n,or,valuerange,pixelcoord) {
-	if(length(valuerange)!=2&&length(pixelcoord)!=2) return(NULL);
-	axtype=1;
-	if(or=="x") or=0;
-	if(or=="y") or=1;
-	a=.jnew("org/rosuda/ibase/toolkit/Axis",.jnull("org/rosuda/ibase/SVar"),as.integer(or),as.integer(axtype));
-	.jcall(a,,"setGeometry",as.integer(or),as.integer(pixelcoord[1]),as.integer(pixelcoord[2]));
-	.jcall(a,"Z","setValueRange",as.double(valuerange[1]),as.double(valuerange[2]));
-	.jcall(pplot$obj,,"setAxis",a);
-	.jcall(a,,"addDepend",.jcast(pplot$obj,"org/rosuda/ibase/Dependent"));
-	gcp=function(pos) {
-		ppos=.jcall(a,"I","getCasePos",as.double(pos));
-		return(ppos);
-	}
-	return(gcp);
-}
-
-iaxis<-function(pplot,n,or,valuerange,pixelcoord) {
-	if(length(valuerange)!=2&&length(pixelcoord)!=2) return(NULL);
-	if(or=="x") or=0;
-	if(or=="y") or=1;
-	a=.jnew("org/rosuda/ibase/toolkit/Axis",.jnull("org/rosuda/ibase/SVar"));
-	.jcall(a,,"setGeometry",as.integer(or),as.integer(pixelcoord[1]),as.integer(pixelcoord[2]));
-	.jcall(a,"Z","setValueRange",as.double(valuerange[1]),as.double(valuerange[2]));
-	.jcall(pplot$obj,,"setAxis",a);
-	.jcall(a,,"addDepend",.jcast(pplot$obj,"org/rosuda/ibase/Dependent"));
-	gcp=function(pos) {
-		if(.jcall(v,"Z","isCat")==T) {ppos=.jcall(a,"I","getCasePos",as.double(pos)); return(ppos);}
-		else if(.jcall(v,"Z","isNum")==T) {ppos=.jcall(a,"I","getValuePos",as.double(pos)); return(ppos);}
-	}
-	return(gcp);
-}
-
-# data(iris);
-# dat=iris[1:2];
-# dat[[2]]=factor(as.integer(runif(150,0,2)));levels(dat[[2]])<-c("M","W")
-# dat[[1]]=dat[[1]]*13-30
-
-# pplot=.iplots[[iplot.cur()]];
-
-icustom.plot<-function(name,min.data.dim,param,construct) {
-	
-	f<-function(vars,...) {
-	  vv<-vector()
-	  i <- 1
-	  for (var in vars) {
-    	if (length(var) > 1) {
-	      varname <- names(vars)[[i]]
-	      if (!is.null(varname))
-		      var <- ivar.new(.ivar.valid.name(varname), var)
-		  else
-		      var <- ivar.new(.ivar.valid.name("customVar"), var)
-	      if (inherits(var,"ivar"))  vv <- c(vv,var$vid)
-	    }
-	    i <- i+1
- 	 }
-	  if (length(list(...))>0) iplot.opt(...,plot=a)
-		lastPlot<-.jcall(.iplots.fw,"Lorg/rosuda/ibase/plots/CustomCanvas;","newCustomplot",as.integer(vv),"");
-		
-		a<-iplot.new(lastPlot);
-		return(a);
-	}
-	
-}
-
-iagepyr.caller<-function(plot,width,height,data) {
-	iagepyr.definition$construct(plot,width,height,data);
-}
-
-iagepyr.definition=list(name="Alterspyramide",min.data.dim=2,param=list(bin.width=10),
-		construct=function(plot,width,height,data)	 {
-			print("Bin im construct");
-			bin.width=iagepyr.definition$param$bin.width;
-		    side=unclass(data[[2]])
-		    left=which(side==1)
-		    right=which(side==2)
-   			anchor=min(data[[1]])
-    		bins=ceiling(max(data[[1]])/bin.width)
-		    bin=ceiling((data[[1]]-anchor)/bin.width)
-		   	largest.bin=max(table(bin))
-
-##          v1<-c()
-##			for(i in seq(1,length(left))) v1<-c(v1,data[[left[i],1]])
-##			v2<-c()
-##			for(i in seq(1,length(right))) v2<-c(v2,data[[right[i],1]])
-##	   	    a.l=iaxiscont(plot, 1, "x", c(0, largest.bin), c(width/2, 10), v1)
-## 	        a.r=iaxiscont(plot, 2, "x", c(0, largest.bin), c(width/2, width-10), v2)
-
-   	   	    a.l=iaxiscont(plot, 1, "x", c(0, largest.bin), c(width/2, 10))
-   	        a.r=iaxiscont(plot, 2, "x", c(0, largest.bin), c(width/2, width-10))
-            a.y=iaxiscont(plot, 3, "y", c(0, bins), c(0,100))
-            
-			.iplots.cp.resetPP(plot);
-            for (i in 1:bins) {
-	        	ids.l=which(bin==i & side==1)
-	        	ids.r=which(bin==i & side==2)
-     	    	if (length(ids.l)>0) {
-     	    		x=2*a.l(0)-a.l(length(ids.l));
-     	    		y=a.y(i-0.5);
-     	    		w=a.l(length(ids.l))-a.l(0);
-     	    		h=a.y(i+0.5)-a.y(i-0.5);
-					new.PPrimRect(plot,x,y,w,h, as.integer(3), id=ids.l);
-				}
-				if(length(ids.r)>0) {
-					x=a.r(0);
-					y=a.y(i-0.5);
-					w=a.r(length(ids.r))-a.r(0);
-					h=a.y(i+0.5)-a.y(i-0.5);
-					new.PPrimRect(plot,x,y,w,h,as.integer(1),id=ids.r);
-				}
-        		
-		    }
-		});
-
-# iagepyr<-icustom.plot(iagepyr.definition);
