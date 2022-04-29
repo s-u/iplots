c22495c3696b270dfed632408152b788750fcd4b
Simon Urbanek <simon.urbanek@R-project.org>
2022-04-29 17:52:51 +1200

    fix typo in macOS warning

--- R/iplots.R
+++ R/iplots.R
@@ -101,7 +101,7 @@ setClass("ivar", representation(obj="jobjRef", vid="integer", name="character", 
               .jcall("java/lang/System","S","setProperty","register.preferences","false")
               .jcall("java/lang/System","S","setProperty","register.quit","false")
           }
-          ipe$.issue.warning <- "Note: On Mac OS X we strongly recommend using iplots from within JGR.\nProceed at your own risk as iplots cannot resolve potential ev.loop deadlocks.\n'Yes' is assumed for all dialogs as they cannot be shown without a deadlock,\nalso ievent.wait() is disabled.\nMore recent OS X version do not allow signle-threaded GUIs and will fail.\n"
+          ipe$.issue.warning <- "Note: On macOS we strongly recommend using iplots from within JGR.\nProceed at your own risk as iplots cannot resolve potential ev.loop deadlocks.\n'Yes' is assumed for all dialogs as they cannot be shown without a deadlock,\nalso ievent.wait() is disabled.\nMore recent macOS versions do not allow single-threaded GUIs and will fail.\n"
       } else {
           # don't mess JGR up
           .jcall("java/lang/System","S","setProperty","register.about","false")
